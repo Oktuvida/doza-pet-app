650c83aaeef6b8e271446487c80de673b438aad4
Diryon Mora <oktuvida@hotmail.com>
2023-03-26 23:33:42 -0500

    refactor: FormButton to extend BaseButton and remove hard-coded text

--- lib/widgets/buttons/form_button.dart
+++ lib/widgets/buttons/form_button.dart
@@ -1,13 +1,11 @@
+import 'package:doza_pet/widgets/abstracts/base_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class FormButton extends StatelessWidget {
-  final void Function() onPressed;
-  const FormButton({super.key, required this.onPressed});
+class FormButton extends BaseButton {
+  const FormButton({super.key, required super.onPressed, required super.child});
 
   @override
   Widget build(BuildContext context) {
-    var localizations = AppLocalizations.of(context);
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
@@ -17,7 +15,7 @@ class FormButton extends StatelessWidget {
           shape: MaterialStateProperty.all(
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))),
         ),
-        child: Text(localizations!.continueMessage),
+        child: child,
       ),
     );
   }
