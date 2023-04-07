6a5916cf0f0e990c2155ebcf101ac42d97b2ab28
Diryon Mora <oktuvida@hotmail.com>
2023-04-07 03:21:19 -0500

    fix: change `appBar` UI to minimalist one

--- lib/constants/ui_constants.dart
+++ lib/constants/ui_constants.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'app_constants.dart';
-
 class UIConstants {
-  static AppBar appBar(context) {
-    var theme = Theme.of(context);
-
-    return AppBar(
-      title: const Text(AppConstants.name),
-      backgroundColor: theme.colorScheme.primary,
-      foregroundColor: theme.colorScheme.onPrimary,
-    );
+  static AppBar appBar() {
+    return AppBar();
   }
 }
