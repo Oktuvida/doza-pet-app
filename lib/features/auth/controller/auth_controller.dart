3ce9f5d5a81176bb2a4fdae3d6b3f38591892bf4
Diryon Mora <oktuvida@hotmail.com>
2023-04-07 05:35:24 -0500

    feat(auth): add Auth Controller

---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,27 @@
+import 'package:doza_pet/apis/auth_api.dart';
+import 'package:doza_pet/core/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final authControllerProvider =
+    StateNotifierProvider<AuthController, bool>((ref) {
+  return AuthController(authAPI: ref.watch(authAPIProvider));
+});
+
+class AuthController extends StateNotifier<bool> {
+  final AuthAPI _authAPI;
+
+  AuthController({required AuthAPI authAPI})
+      : _authAPI = authAPI,
+        super(false);
+
+  void signUp(
+      {required String username,
+      required String password,
+      required BuildContext context}) async {
+    state = true;
+    final res = await _authAPI.signUp(username: username, password: password);
+
+    res.fold((l) => showSnackBar(context, l.message), (r) => print(r.username));
+  }
+}
