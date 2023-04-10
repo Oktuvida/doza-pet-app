a3edffff4f9c445bad924a8a1cdfc24275207e84
Diryon Mora <oktuvida@hotmail.com>
2023-04-10 01:30:00 -0500

    feat: define route constants

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,3 +1,4 @@
 export "./app_constants.dart";
 export "./theme_constants.dart";
 export "./ui_constants.dart";
+export "./route_constants.dart";
---
+++ lib/constants/route_constants.dart
@@ -0,0 +1,5 @@
+abstract class RouteConstants {
+  static const String home = "/home";
+  static const String signIn = "/auth/signIn";
+  static const String signUp = "/auth/signUp";
+}
